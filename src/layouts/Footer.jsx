5611ac3f57ec80e5516777aed18d3a0894ce6e45
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-04-12 03:03:41 -0300

    feat: initial section incomplete (1/2) stage commit

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
 import { GitHub, LinkedIn } from '@mui/icons-material';
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Alert from 'react-bootstrap/Alert';
+import logoByteStyle from '../images/logoByteStyle.webp';
 
 export default function Footer() {
+    const [showAlert, setShowAlert] = useState(false);
+
+    // Função para exibir o alerta
+    const handleAlert = () => {
+        setShowAlert(true);
+    };
+
+    // Função para fechar o alerta
+    const handleCloseAlert = () => {
+        setShowAlert(false);
+    };
+
     return (
-        <footer className="bg-dark text-light py-4">
+        <footer className="bg-dark text-light py-4" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', textAlign: 'center'}}>
+            <img src={logoByteStyle} alt="Logotipo da marca ByteStyle" width="200" height="200" />
             <Container>
                 <Row>
                     <Col md={4}>
@@ -15,12 +30,12 @@ export default function Footer() {
                     </Col>
                     <Col md={4}>
                         <h4>Redes sociais</h4>
-                        <a href="https://github.com/matheusmartinsviana/byte_de_estilo" target="_blank" rel="noopener noreferrer" style={{ color: '#007bff' }}>
-                            <GitHub /> ownersByteStyle
-                        </a>
-                        <br />
                         <a href="https://www.linkedin.com/in/matheusmartinsviana/" target="_blank" rel="noopener noreferrer" style={{ color: '#007bff' }}>
                             <LinkedIn /> owner
+                        </a>
+                        <br />
+                        <a href="https://github.com/matheusmartinsviana/byte_de_estilo" target="_blank" rel="noopener noreferrer" style={{ color: '#007bff' }}>
+                            <GitHub /> ownersByteStyle
                         </a>
                     </Col>
                     <Col md={4}>
@@ -32,13 +47,18 @@ export default function Footer() {
                         </ul>
                     </Col>
                 </Row>
-            </Container>
             <div className="text-center mt-3">
                 <p>&copy; 2024 ByteStyle. Todos os direitos reservados.</p>
-                <a href="##" style={{ color: '#007bff' }}>Política de Privacidade</a>
-
-
+                <a href="##" style={{ color: '#007bff' }} onClick={handleAlert}>Política de Privacidade</a>
             </div>
+            </Container>
+
+            {/* Renderização condicional do alerta */}
+            {showAlert && (
+                <Alert variant="success" onClose={handleCloseAlert} dismissible>
+                    Maybe you should read the privacy policy before continuing.
+                </Alert>
+            )}
         </footer>
     );
 }
--- src/pages/Initial.jsx
+++ src/pages/Initial.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import orangeTechClothes from '../images/clothes.webp';
+import style from './styles/Initial.module.css';
 
 export default function Initial() {
     return (
-        <div>
-            <h1>Página inicial</h1>
+        <div className={style.initialContainer}>
+            <main className={style.mainSection}>
+                <section className={style.initialSectionLeft}>
+                    <h1>Inovação em cada confecção</h1>
+                    <p>Conheça nossas roupas tecnológicas</p>
+                    <button>Confira</button>
+                </section>
+                <section className={style.initialSectionRight}>
+                    <img src={orangeTechClothes} alt="Roupas tecnológicas" />
+                </section>
+            </main>
         </div>
-    )
+    );
 }
